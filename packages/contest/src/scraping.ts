cdde31fa3a7a1bedf0f5e62755a605711c9a628d
shun-shobon <shun.shobon@gmail.com>
2020-07-19 15:59:00 +0900

    feat(contest): add fetchPage()

--- packages/contest/src/scraping.ts
+++ packages/contest/src/scraping.ts
@@ -1,4 +1,14 @@
-/* eslint-disable */
-export async function fetchPage() {}
+import axios from "axios";
 
+export async function fetchPage(): Promise<string | null> {
+  const url = "https://atcoder.jp/contests/?lang=ja";
+  try {
+    const { data } = await axios.get<string>(url);
+    return data;
+  } catch (error) {
+    return null;
+  }
+}
+
+/* eslint-disable */
 export function parseUpcomingContest(html: string) {}
